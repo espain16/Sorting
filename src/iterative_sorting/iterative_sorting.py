10591edade32f71e7aadd18aaab8f1e0a8ca010d
Elise Spain <elisespain@gmail.com>
2020-02-17 22:41:06 -0800

    Secondary for loop created

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,6 +2,10 @@
 def selection_sort( arr ):
     for i in range(len(arr)):#create starting point 
         lowest_num = arr[i]#create my lowest number variable and assign it to my starting point
+        lowest_ind = i # created a variable to hold the lowest index which is currently i
+        for j in range( i + 1, len(arr) ):# now I need to check the next index in the array and compare each element in the array
+            if arr[j] < lowest_num:#start your comparison of arr[j] to arr[i]
+                lowest_num = arr[j]#switch the variable value if the above statement is true
     
 
 
